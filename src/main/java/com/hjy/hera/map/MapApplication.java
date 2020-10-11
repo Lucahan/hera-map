bafb1644f37757495c1946a4e73a176a455bad65
lucahan <lucahan@foxmai.com>
2020-10-11 20:34:45 +0800

    update

---
+++ src/main/java/com/hjy/hera/map/MapApplication.java
@@ -0,0 +1,13 @@
+package com.hjy.hera.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapApplication.class, args);
+    }
+
+}
